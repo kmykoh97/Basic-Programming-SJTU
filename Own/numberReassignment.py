b9efaa46900130f01355c1bf9f8c106cb5c98119
Koh Meng Yit <kmykoh97@outlook.com>
2017-09-19 06:59:00 +0800

    Added new project

--- Own/numberReassignment.py
+++ Own/numberReassignment.py
@@ -2,11 +2,13 @@
 #Created by MyKoh
 #2017-09-18
 
+#Telling purpose and requesting user's input
 print 'Hello, this is a number reassignment in assending order program'
 userInput = input('Please give numbers separated by comma')
 x = list(userInput)
 print 'Your given numbers are', x
 
+#Create functin by loop
 def numbersReassignment():
     initialCount = 0
     finalCount = len(x)
@@ -20,3 +22,4 @@ def numbersReassignment():
     print 'After sorting, the assending arrangement is:', x
 
 numbersReassignment()
+print 'Have a good day'
--- Own/quadraticEquationSolving.py
+++ Own/quadraticEquationSolving.py
@@ -1,1 +1,36 @@
+#Project to solve quadratic equation
+#Created by MyKoh
+#2017-09-19
 
+#requesting user input:
+print 'Hello, this is a program to solve quadratic equation'
+print 'ax^2 + bx + c = 0'
+
+def quadraticEquation():
+    while True:
+        #requesting user input:
+        a = input('Please insert your a')
+        b = input('Please insert your b')
+        c = input('Please insert your c')
+        x = b ** 2 - 4 * a * c
+
+        if x < 0:
+            print 'It has no real roots'
+        elif x == 0:
+            print 'Your equation has 2 same roots which are:', (-b + (x//2))/2*a
+        else:
+            print 'The solutions are:', ((-b + (x//2))/2*a), 'and', (-b - (x//2))/2*a
+
+        NewCalculation = raw_input('Thank you for using me, do you want to start a new calculation?(y or n)')
+        if NewCalculation == 'y':
+            print 'OK'
+        elif NewCalculation == 'n':
+            break
+        else:
+            #I can make one more loop here. Try again next time
+            print "I don't understand you. I will assume you want to terminate program."
+            break
+
+
+quadraticEquation()
+print 'Have a good day'
