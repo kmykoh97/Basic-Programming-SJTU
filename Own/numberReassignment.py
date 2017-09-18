f5537228fa95f93b2c5814c02eee3509ab5f25d5
Koh Meng Yit <kmykoh97@outlook.com>
2017-09-19 05:35:55 +0800

    Initial code publish

---
+++ Own/numberReassignment.py
@@ -0,0 +1,22 @@
+#Number Reasignment Project
+#Created by MyKoh
+#2017-09-18
+
+print 'Hello, this is a number reassignment in assending order program'
+userInput = input('Please give numbers separated by comma')
+x = list(userInput)
+print 'Your given numbers are', x
+
+def numbersReassignment():
+    initialCount = 0
+    finalCount = len(x)
+    while initialCount < finalCount ** 2:
+        for i in range(len(x)-1):
+            if x[i] > x[i+1]:
+                x[i], x[i+1] = x[i+1], x[i]
+            else:
+                initialCount += 1
+                
+    print 'After sorting, the assending arrangement is:', x
+
+numbersReassignment()
