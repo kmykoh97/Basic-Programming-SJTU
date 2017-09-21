37df289b3548fb1ec893501f81d2f4c7da35134f
Koh Meng Yit <kmykoh97@outlook.com>
2017-09-21 11:24:15 +0800

    Added function

--- Own/GregorianEpact .py
+++ Own/GregorianEpact .py
@@ -3,7 +3,10 @@
 #Created by MyKoh
 #2017-09-20
 
-year = input('Please enter a 4-digits year')
-c = year / 100
-epact = (8 + (c/4) - c + ((8*c + 13) / 25) + 11 * (year % 19)) % 30
-print epact
+def gregorianEpact():
+    year = input('Please enter a 4-digits year')
+    c = year / 100
+    epact = (8 + (c/4) - c + ((8*c + 13) / 25) + 11 * (year % 19)) % 30
+    print epact
+
+gregorianEpact()
