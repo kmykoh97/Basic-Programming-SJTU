fee800f669b73e4ec321635295a389216d8b453e
Koh Meng Yit <kmykoh97@outlook.com>
2017-10-11 00:07:50 +0800

    Added new project

---
+++ Own/stringLowerNumberDeletion.py
@@ -0,0 +1,16 @@
+#A project to change big letter strings to small and delete numbers then return them
+#Created by MyKoh
+#2017-10-10
+
+def stringLowerNumberDeletion():
+    userInsert = raw_input('Please insert:')
+    for i in userInsert:
+        if i in "1234567890":
+            userInsert = userInsert.replace(i,'')
+        
+    print userInsert.lower()
+
+stringLowerNumberDeletion()
+            
+        
+
