3e91401f6389ac44c6ca1caf8196ee0b31b97f86
Koh Meng Yit <kmykoh97@outlook.com>
2017-10-19 00:11:06 +0800

    Added HCF function

--- Own/HCFLCM.py
+++ Own/HCFLCM.py
@@ -1,12 +1,12 @@
-#A program to find HCF, LCM
+#A program to find LCM, HCF
 #Created by MyKoh
 #2017-10-20
 
 #Input: Integer a, b, c
 #Output: HCF, LCM of a, b, c
 
-def main():
-    a, b, c = input('Please insert 3 numbers saparated by ,:')
+def LCM():
+    #a, b, c = input('Please insert 3 numbers saparated by ,:')
     truthValue = True
     multipleA, multipleB, multipleC = 1, 1, 1
     newA, newB, newC = a, b, c
@@ -23,10 +23,52 @@ def main():
         if newA - newB == 0 and newA - newC == 0:
             truthValue = False
 
+    
+
     print 'LCM is', str(newA)
         
-
    #alternative LCM: use lower number in that 3 and add 1 by 1 and if x % y== 0, lcm 
    #alternative: 3x for loops
+
+#LCM()
+
+def HCF():
+    #a, b, c = input('Please insert 3 numbers saparated by ,:')
+    listA = []
+    listB = []
+    listC = []
+    resultList = []
+    for i in range(1, a+1):
+        for j in range(i, a+1):
+            if i * j == a:
+                listA.append(i)
+                listA.append(j)
+
+    for i in range(1, b+1):
+        for j in range(i, b+1):
+            if i * j == b:
+                listB.append(i)
+                listB.append(j)
+
+    for i in range(1, c+1):
+        for j in range(i, c+1):
+            if i * j == c:
+                listC.append(i)
+                listC.append(j)
+
+    for i in listA:
+        for j in listB:
+            for k in listC:
+                if i == j == k:
+                    resultList.append(i)
+
+    print 'HCF is', str(max(resultList))
+
+#HCF()
+
+def main():
+    a, b, c = input('Please insert 3 numbers saparated by ,:')
+    LCM()
+    HCF()
 
 main()
