8b19c0e74eaea7f415c385e450d37d5fb56dfdd4
kmykoh97 <kmykoh97@gmail.com>
2017-11-06 18:48:10 +0800

    Finished code

--- Own/worldTime.py
+++ Own/worldTime.py
@@ -5,44 +5,35 @@
 #Using class
 class worldTime:
 
-    def _init_(self, hour, minutes, seconds):
+    def __init__(self, UTC, hour, minutes, seconds):
+        self.UTC = UTC
         self.hour = hour
         self.minutes = minutes
         self.seconds = seconds
 
-    def getTime(self):
-        self.hour = input('Insert hour')
-        self.minutes = input('Insert minutes')
-        self.seconds = input('Insert seconds')
-
-    def timeConversion(self, city):
-        self.city = raw_input("City:")
-        countryList = ['london', 'japan', 'italy', 'german', 'new york']
-        UTC = [0, 9, 1, 1, 5]
-        for i in range(len(countryList)):
-            if countryList[i].lower() == self.city:
-                self.hour = self.hour - 8 + UTC[i]
-
-    def day(self):
+    def timeConversion(self):
+        self.hour = self.hour - 8 + self.UTC
         if self.hour < 0:
             print 'It is previous day'
             self.hour += 24
         elif self.hour > 24:
             print 'It is the next day'
             self.hour -= 24
+        print "%s is %d.%d.%d now" % (str(x), self.hour, self.minutes, self.seconds)
 
-    def printTime(self):
-        print "%s is %d.%d.%d now" % (self.city, self.hour, self.minutes, self.seconds)
-
-time = worldTime()
 print "Please input Beijing time in 24-hour format"
 print
-time.getTime()
-time.city = raw_input("City:")
-time.timeConversion()
-time.day()
-time.printTime()
+a = input("Please insert hour in Beijing now:")
+b = input('Please insert minutes in Beijing now:')
+c = input('Please insert seconds in Beijing now:')
+london = worldTime(0,a,b,c)
+japan = worldTime(9,a,b,c)
+italy = worldTime(1,a,b,c)
+german = worldTime(1,a,b,c)
+newyork = worldTime(5,a,b,c)
+x = input('Insert your country:')
 
+x.timeConversion()
 
 #Using function
 def main():
@@ -54,3 +45,5 @@ def main():
         if countryList[i].lower() == x:
             newTime = beijingTime - 8 + UTC[i]
     return newTime
+
+#main()
