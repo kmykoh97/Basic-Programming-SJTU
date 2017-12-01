0da2a35ec40dc3fb2af5c886f1db29bf18b5e7fd
kmykoh97 <kmykoh97@gmail.com>
2017-12-02 00:46:26 +0800

    Improve code

--- School/2017上机/4rd lesson/class3.py
+++ School/2017上机/4rd lesson/class3.py
@@ -17,9 +17,30 @@ class blackJack:
 
     def getCard(self):
         shuffle(self.cardDeckList)
-        self.
+        self.pop(0)
+
+    def cardValue(self, x):
+        if x=='2' or x=='3' or x=='4' or x=='5' or x=='6' or x=='7' or x=='8' or x=='9' or x=='10':
+            return int(x)
+        elif x == 'J' or x == 'Q' or x == 'K':
+            return 10
+        else:
+            return 11
 
     def bot(self):
+        score = 0
+        while score < 17:
+            score = 0
+            self.botList.getCard()
+            print "Bot now have:", len(self.botList) * X
+            for i in self.botList:
+                score += cardValue(i)
+            if score > 21:
+                for i in cardList:
+                    if i == 'A':
+                        score -= 10
+
+        return score
 
     def player(self):
 
