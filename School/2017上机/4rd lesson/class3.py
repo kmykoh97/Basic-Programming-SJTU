67308b1f5d9c1868d60fac2ab0d5d782b03d83a1
kmykoh97 <kmykoh97@gmail.com>
2017-12-02 00:40:29 +0800

    Modified code

--- School/2017上机/4rd lesson/class3.py
+++ School/2017上机/4rd lesson/class3.py
@@ -1,5 +1,29 @@
+# A redo of blackjack game using class
+
 from random import *
 
 class blackJack:
 
-    def __init__(self, botList, playerList, )
+    def __init__(self):
+        self.numberOfDeck = 2
+        self.botList = []
+        self.playerList = []
+        self.cardDeckList = numberOfDeck * 4 * ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+    def initialShuffleAndDistribute(self):
+        shuffle(self.cardDeckList)
+        self.botList.pop(0)
+        self.playerList.pop(0)
+
+    def getCard(self):
+        shuffle(self.cardDeckList)
+        self.
+
+    def bot(self):
+
+    def player(self):
+
+
+def main():
+
+main()
