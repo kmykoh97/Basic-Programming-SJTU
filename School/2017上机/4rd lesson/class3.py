951bcb97ca748d9821b2bb0853c5fb888c1caf6f
kmykoh97 <kmykoh97@gmail.com>
2017-12-02 15:54:46 +0800

    Make code almost perfect

--- School/2017上机/4rd lesson/class3.py
+++ School/2017上机/4rd lesson/class3.py
@@ -4,20 +4,22 @@ from random import *
 
 class blackJack:
 
-    def __init__(self):
-        self.numberOfDeck = 2
+    def __init__(self, x):
+        self.numberOfDeck = x
         self.botList = []
         self.playerList = []
-        self.cardDeckList = numberOfDeck * 4 * ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        self.cardDeckList = self.numberOfDeck * 4 * ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
     def initialShuffleAndDistribute(self):
         shuffle(self.cardDeckList)
-        self.botList.pop(0)
-        self.playerList.pop(0)
+        self.botList.append(self.cardDeckList.pop())
+        self.botList.append(self.cardDeckList.pop())
+        self.playerList.append(self.cardDeckList.pop())
+        self.playerList.append(self.cardDeckList.pop())
 
-    def getCard(self):
+    def getCard(self, other):
         shuffle(self.cardDeckList)
-        self.pop(0)
+        other.append(self.cardDeckList.pop())
 
     def cardValue(self, x):
         if x=='2' or x=='3' or x=='4' or x=='5' or x=='6' or x=='7' or x=='8' or x=='9' or x=='10':
@@ -27,24 +29,66 @@ class blackJack:
         else:
             return 11
 
+    def winCondition(self):
+        if dealer > 21 or player1 > dealer:
+            return 1
+        elif player1 == dealer:
+            return 2
+        else:
+            return 0
+
     def bot(self):
         score = 0
         while score < 17:
             score = 0
-            self.botList.getCard()
-            print "Bot now have:", len(self.botList) * X
+            self.getCard(self.botList)
+            print "Bot now have:", len(self.botList) * 'X'
             for i in self.botList:
-                score += cardValue(i)
+                score += self.cardValue(i)
             if score > 21:
-                for i in cardList:
+                for i in self.botList:
                     if i == 'A':
                         score -= 10
-
         return score
 
     def player(self):
+        question = 'y'
+        while question == 'y' or question == 'Y':
+            print 'Haha, the cards u got are:', self.playerList
+            question = raw_input('Do you want to draw another card? (y or n)')
+            if question == 'y' or question == 'Y':
+                self.getCard(self.playerList)
+        print 'Lastly, your cards are:', self.playerList
+        score = 0
+        for i in self.playerList:
+            score += self.cardValue(i)
+        y = 0
+        while score > 21 and y <= (len(self.playerList)-1):
+            if self.playerList[y] == 'A':
+                score -= 10
+            y += 1
+        return score
 
+    def gameStart(self):
+        self.initialShuffleAndDistribute()
+        print 'Your turn to draw card...'
+        player1 = self.player()
+        print "Dealer's turn to draw card..."
+        dealer = self.bot()
+        print "Dealer's card:", self.botList
+        if self.winCondition() == 0:
+            print 'Player lost...'
+        elif self.winCondition() == 1:
+            print 'Congratulations, player wins'
+        else:
+            print "This game is a draw. Let's have another game XD"
+
+
+        self.winCondition()
 
 def main():
+    x = input('please insert number of card deck to use:')
+    firstPlayer = blackJack(x)
+    firstPlayer.gameStart()
 
 main()
