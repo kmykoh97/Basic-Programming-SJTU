f17582de74090d0d78343f28e1bb931c2c7a876c
kmykoh97 <kmykoh97@gmail.com>
2018-03-19 00:59:36 +0800

    fixed many bugs

--- School/c++/lab1/calculator.cpp
+++ School/c++/lab1/calculator.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <cmath>
+#include <string>
 
 using namespace std;
 
-// do some init of classes and functions here
+// do some definitions here
 void calculate();
 double expression();
+bool decimalsDetermine(double x);
 double term();
 double primary();
 int factorial(int a);
@@ -16,13 +18,7 @@ const char quit = 'q';
 const char print = ';';
 const string prompt = "> ";
 const string result = "= ";
-
-// error handling
-void error(string info)
-{
-    // cout << info << endl;
-    cerr << info << endl;
-}
+int main();
 
 // make a new type of token
 struct token
@@ -36,29 +32,28 @@ class tokenStream
 {
     public:
       token get(); // get a token from expression if there is nothing in buffer. Else,
-      void ignore(char c);
-      void putBack(token); // place a token to buffer
+      void restart();
+      void putBack(token t); // place a token to buffer
 
     private:
       bool full {false}; // determine whether the buffer has been filled
       token buffer; // allocate a memmory to store unused and returned token
 };
 
-void tokenStream::ignore(char c)
-{
-    if (full && c == buffer.kind) {
-        full = false;
-        return;
-    }
-    full = false;
+tokenStream ts;
 
-    char ch;
-    while (cin >> ch) {
-        if (ch == c) {
-            return;
-        }
-    }
+// error type
+class badToken{
+};
+
+// error handling
+void error(string info)
+{
+    cerr << info << endl;
+    throw badToken{};
 }
+
+
 
 // define putBack method here
 void tokenStream::putBack(token t)
@@ -89,10 +84,9 @@ token tokenStream::get()
         case '+': case '-':
         case '*': case '/':
         case '(': case ')':
-        case quit:
-        case print:
+        case quit: case print:
+        case '%': case '!':
             return token{temp};
-        case '.':
         case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
             {
                 cin.putback(temp); // put the char back into cin
@@ -105,11 +99,14 @@ token tokenStream::get()
     }
 }
 
-tokenStream ts;
-
-void cleanUp()
+void tokenStream::restart()
 {
-    ts.ignore(print);
+    string temp;
+    getline(cin, temp);
+    if (temp.empty()) {
+        return;
+    }
+    cin >> temp;
 }
 
 // check for brackets or return values
@@ -118,7 +115,7 @@ double primary()
     token t = ts.get(); // get the next token
 
     switch (t.kind) {
-        case '(': // perform operations inside bracket
+        case '(': case ')': // perform operations inside bracket
             {
                 double insideBracket = expression();
                 t = ts.get();
@@ -128,16 +125,36 @@ double primary()
                 return insideBracket;
             }
         case number: // in case of number found
-            return t.value;
+            {
+                double temp = t.value;
+                t = ts.get();
+                if (t.kind == '!') {
+                    temp = factorial((int) temp);
+                } else {
+                    ts.putBack(t);
+                }
+                return temp;
+            }
         case '-':
             return -primary();
         case '+':
             return primary();
         default: // in case of special symbols found
-            error("primary expected");
+            error("special symbols not allowed");
     }
 
     // return t.value;
+}
+
+bool decimalsDetermine(double x)
+{
+    int copier = x;
+
+    if (copier == x) {
+        return false; // no decimal points
+    } else {
+        return true; // has decimal points
+    }
 }
 
 // perform multiplication and division. Perform primary()
@@ -155,17 +172,21 @@ double term()
                 break;
             case '/':
                 {
-                    if (primary() == 0) {
+                    double temp = primary();
+			        if (temp == 0.0) {
                         error("zero error");
-                    } else {
-                        left /= primary();
                     }
-                } break;
+			        left /= temp;
+			        break;
+                }
             case '%':
                 {
                     double temp = primary();
                     if (temp == 0) {
                         error("zero error");
+                    }
+                    if (decimalsDetermine(temp) || decimalsDetermine(left)) {
+                        error("cannot be decimal points");
                     }
                     left = fmod(left, temp);
                     // t = ts.get();
@@ -196,9 +217,12 @@ double expression()
             case '-':
                 left -= term();
                 break;
+            case '!':
+                return factorial((int)left);
             default:
                 /* if putBack not called, need 2x ';' to show result */
-                ts.putBack(t);  // if no '+' or '-' sign, put the token to buffer in class tokenStream() to access later
+                ts.putBack(t); // if no '+' or '-' sign, put the token to buffer in class tokenStream() to access later
+                // return primary();
                 return left;
         }
     }
@@ -224,13 +248,15 @@ void calculate()
 
         catch (exception& e) {
             cerr << e.what() << endl;
-            cleanUp();
         }
     }
 }
 
 int factorial (int a)
 {
+    if (a < 0) {
+        error("cannot be negative factorial");
+    }
     if (a == 1 || a == 0) {
         return 1;
     }
@@ -243,19 +269,13 @@ int main()
 {
     try {
         calculate();
-        system("pause");
-        return 0;
     }
 
-    catch (exception& e) {
-        cerr << e.what() << endl;
-        system("pause");
-        return 1;
+    catch (badToken) {
+        ts.restart();
+        main();
     }
 
-    catch (...) {
-        cerr << "exception \n";
-        system("pause");
-        return 2;
-    }
+    system("pause");
+    return 0;
 }
