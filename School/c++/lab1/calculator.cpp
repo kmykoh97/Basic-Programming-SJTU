7422f89f45a08bddc79b9f10d851460ed501a34b
kmykoh97 <kmykoh97@gmail.com>
2018-03-18 14:48:28 +0800

    completed calculator with basic functionalities

--- School/c++/lab1/calculator.cpp
+++ School/c++/lab1/calculator.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
-#include <vector>
-#include <string>
+#include <cmath>
 
 using namespace std;
+
+// do some init of classes and functions here
+void calculate();
+double expression();
+double term();
+double primary();
+int factorial(int a);
+void cleanUp();
+void error(string info);
+const char number = '8';
+const char quit = 'q';
+const char print = ';';
+const string prompt = "> ";
+const string result = "= ";
 
 // error handling
 void error(string info)
 {
-    cout << info << endl;
-    // cerr << info << endl;
+    // cout << info << endl;
+    cerr << info << endl;
 }
 
 // make a new type of token
@@ -22,13 +35,30 @@ struct token
 class tokenStream
 {
     public:
-      token get(); // get a token from expression if there is nothing in buffer. Else, 
+      token get(); // get a token from expression if there is nothing in buffer. Else,
+      void ignore(char c);
       void putBack(token); // place a token to buffer
 
     private:
       bool full {false}; // determine whether the buffer has been filled
       token buffer; // allocate a memmory to store unused and returned token
 };
+
+void tokenStream::ignore(char c)
+{
+    if (full && c == buffer.kind) {
+        full = false;
+        return;
+    }
+    full = false;
+
+    char ch;
+    while (cin >> ch) {
+        if (ch == c) {
+            return;
+        }
+    }
+}
 
 // define putBack method here
 void tokenStream::putBack(token t)
@@ -46,7 +76,6 @@ token tokenStream::get()
 {
     // initialisation of variables
     char temp;
-    double number;
 
     // get from buffer if there is something there
     if (full) {
@@ -55,24 +84,33 @@ token tokenStream::get()
     }
 
     // if there is nothing in buffer, check for operators or doubles
-    cin >> temp;
+    cin >> temp; // skips spaces
     switch(temp) {
-        case '+': case '-': case '*': case '/': case '(': case ')': case 'q': case ';':
+        case '+': case '-':
+        case '*': case '/':
+        case '(': case ')':
+        case quit:
+        case print:
             return token{temp};
+        case '.':
         case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
-        {
-            cin.putback(temp); // put the char back into cin
-            cin >> number; // get complete double from cin
-            return token{'8', number}; // return a token with suitable attributes
-        }
+            {
+                cin.putback(temp); // put the char back into cin
+                double value;
+                cin >> value; // get complete double from cin
+                return token{number, value}; // return a token with suitable attributes
+            }
         default:
             error("invalid token");
     }
 }
 
-// do some init of class and function here
 tokenStream ts;
-double expression();
+
+void cleanUp()
+{
+    ts.ignore(print);
+}
 
 // check for brackets or return values
 double primary()
@@ -89,8 +127,12 @@ double primary()
                 }
                 return insideBracket;
             }
-        case '8': // in case of number found
+        case number: // in case of number found
             return t.value;
+        case '-':
+            return -primary();
+        case '+':
+            return primary();
         default: // in case of special symbols found
             error("primary expected");
     }
@@ -112,14 +154,23 @@ double term()
                 left *= primary();
                 break;
             case '/':
-            {
-                if (primary() == 0) {
-                    error("zero error");
-                } else {
-                    left /= primary();
+                {
+                    if (primary() == 0) {
+                        error("zero error");
+                    } else {
+                        left /= primary();
+                    }
                 } break;
-            }
-                break;
+            case '%':
+                {
+                    double temp = primary();
+                    if (temp == 0) {
+                        error("zero error");
+                    }
+                    left = fmod(left, temp);
+                    // t = ts.get();
+                    break;
+                }
             default: 
                 ts.putBack(t); // if no '*' or '/' sign, put the token to buffer in class tokenStream() to access later
                 return left;
@@ -146,7 +197,8 @@ double expression()
                 left -= term();
                 break;
             default:
-                // ts.putBack(t);  // if no '+' or '-' sign, put the token to buffer in class tokenStream() to access later
+                /* if putBack not called, need 2x ';' to show result */
+                ts.putBack(t);  // if no '+' or '-' sign, put the token to buffer in class tokenStream() to access later
                 return left;
         }
     }
@@ -154,25 +206,56 @@ double expression()
     return left; // return result
 }
 
+void calculate()
+{
+    while (cin) {
+        try {
+            cout << prompt;
+            token t = ts.get();
+            while (t.kind == print) {
+                t = ts.get(); // eats ';'
+            }
+            if (t.kind == quit) {
+                return;
+            }
+            ts.putBack(t);
+            cout << result << expression() << endl;
+        }
+
+        catch (exception& e) {
+            cerr << e.what() << endl;
+            cleanUp();
+        }
+    }
+}
+
+int factorial (int a)
+{
+    if (a == 1 || a == 0) {
+        return 1;
+    }
+    
+    return a * factorial(a-1);
+}
+
 // main execution
 int main()
 {
-    double answer = 0;
-    while (cin) {
-        token t = ts.get();
-        if (t.kind == 'q') {
-            break;
-        } else if (t.kind == ';') {
-            cout << answer << endl;
-            break;
-        } else {
-            ts.putBack(t);
-        }
-
-        answer = expression();
+    try {
+        calculate();
+        system("pause");
+        return 0;
     }
 
-    system("pause"); // to view result
+    catch (exception& e) {
+        cerr << e.what() << endl;
+        system("pause");
+        return 1;
+    }
 
-    return 0;
+    catch (...) {
+        cerr << "exception \n";
+        system("pause");
+        return 2;
+    }
 }
