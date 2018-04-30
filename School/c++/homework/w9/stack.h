2b5173d8ee9e860850f901d8bc836dc0fe3dd056
kmykoh97 <kmykoh97@gmail.com>
2018-05-01 00:20:15 +0800

    added stack.h

---
+++ School/c++/homework/w9/stack.h
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <memory>
+
+using namespace std;
+
+template <typename T>
+struct Node
+{
+    T value;
+    shared_ptr<Node> next;
+    Node(T v) : value(v), next(NULL) {} // constructor
+};
+
+template <typename T>
+class Stack
+{
+    private:
+    shared_ptr<Node<T>> head;
+
+    public:
+    Stack() : head(NULL) {} // empty constructor
+    Stack(const Stack<T>&) = delete;
+    Stack& operator=(const Stack<T>&) = delete;
+    void push(T elem); // to push elem to stack
+    bool pop(T& cell); // to remove item from stack and give the value to cell
+    // void print() const; // to print items
+};
+
+template <typename T>
+void Stack<T>::push (T elem)
+{
+    shared_ptr<Node<T>> tempPtr = make_shared<Node<T>>(elem); // smart pointer
+
+    if (head == NULL) {
+        head = tempPtr;
+    } else {
+        tempPtr -> next = head;
+        head = tempPtr; // make the new Node becomes head
+    }
+}
+
+template <typename T>
+bool Stack<T>::pop (T& cell)
+{
+    if (head == nullptr) {
+        return false;
+    } else {
+        cell = head -> value; // give value to cell before deleting
+        head = head -> next;
+    }
+
+    return true;
+}
+
+// method to print items in stack
+/*
+template <typename T>
+void Stack<T>::print() const
+{
+    for (auto temp = head; temp != nullptr; temp = temp -> next) {
+        cout << temp -> value << endl;
+    }
+}
+*/
