bb44f3c4858c3a4a2908e3fd0dc45258042b1644
kmykoh97 <kmykoh97@gmail.com>
2018-07-28 15:06:39 +0800

    added new project (mini Database Management System)

---
+++ School/c++/final/test/sizeTest.cpp
@@ -0,0 +1,17 @@
+/*
+program to test char and int size of your computer
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "size of int: " << sizeof(int) << endl;
+    cout << "size of char: " << sizeof(char) << endl;
+
+    system("pause");
+    return 0;
+}
